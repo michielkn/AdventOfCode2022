dc3904e138aeb4f86632c43f8804627341a07daf
Michiel Knevels <mknevels@qcify.com>
2022-12-05 13:32:27 +0100

    Day 5

---
+++ src/day-5/Day5-2.ts
@@ -0,0 +1,72 @@
+import {readFileSync} from "fs";
+
+const data = String(readFileSync('./data/day-5/data'));
+
+let didReadAllStartingLines = false;
+const startingLines = [];
+const commandLines = [];
+
+const buckets = [];
+
+// read the data
+for(const line of data.split('\n')) {
+    if(line.length === 0) {
+        didReadAllStartingLines = true
+        continue;
+    }
+
+    if(!didReadAllStartingLines) {
+        startingLines.unshift(line);
+    } else {
+        commandLines.push(line);
+    }
+}
+
+let didCreateBuckets = false;
+
+// creating the buckets
+for(const line of startingLines) {
+    if(!didCreateBuckets) {
+        for(const c of line.replaceAll(' ', '')) {
+            buckets.push([]);
+        }
+        didCreateBuckets = true;
+    } else {
+        for(let bucketNumber = 0; bucketNumber < buckets.length ; bucketNumber++) {
+            const partForBucketStartIndex = bucketNumber * 4 + 1
+            const char = line.substring(partForBucketStartIndex, partForBucketStartIndex + 1).trim();
+            if(char.length === 1) {
+                buckets[bucketNumber].push(char);
+            }
+        }
+    }
+}
+
+
+for(const line of commandLines) {
+    const [_move, amountString, _from, fromString, _to, toString] = line.split(' ');
+    const amount = Number(amountString);
+    const from = Number(fromString);
+    const to = Number(toString);
+
+    let movingValues = []
+    for(let i = 0 ; i < amount ; i++) {
+        const val = buckets[from -1].pop();
+        movingValues.push(val);
+    }
+
+    movingValues = movingValues.reverse();
+
+    for(let i = 0; i < movingValues.length; i++) {
+        const val = movingValues[i];
+        buckets[to -1].push(val);
+    }
+}
+
+
+let result = '';
+for(const bucket of buckets) {
+    result+= bucket[bucket.length -1];
+}
+
+console.error(result);
