9d547814deb9d4f66e7d69ae8d289fd34a5cc7fe
Michiel Knevels <mknevels@qcify.com>
2022-12-06 19:05:04 +0100

    Update Day6.ts

--- src/day-6/Day6.ts
+++ src/day-6/Day6.ts
@@ -10,7 +10,7 @@ const data = String(readFileSync('./data/day-6/data'));
 
 for(const line of data.split('\n')) {
     let result = null;
-    for(let i = 3; i < line.length ; i++) {
+    for(let i = amount - 1; i < line.length ; i++) {
         const set = new Set();
         for(let j = 0 ; j < amount ; j++) {
             set.add(line[i - j]);
